70ca7644beec4620dd4b4cd5877fcaca1ce9e9fc
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2023-03-24 11:26:13 +0200

    refactored code: made array of objrct instead array of strings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+
 import "./App.css";
 import TodoForm from "./components/Todos/TodoForm";
 import TodoList from "./components/Todos/TodoList";
@@ -7,11 +9,16 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodoHandler = (text) => {
-    setTodos((prevTodos) => [...prevTodos, text]);
+    const newTodo = {
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
 
-  const deleteTodoHandler = (index) => {
-    setTodos(todos.filter((_, i) => i !== index));
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
   return (
     <div className="App">
